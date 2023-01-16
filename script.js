9438a0f1895e7dcf110cea5b4c0ddd43a24b092b
Rohit Prajapati <65508466+rohu0510@users.noreply.github.com>
2023-01-16 21:57:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const textbox = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+textbox.addEventListener("input", function() {
+  letterCount.innerHTML = textbox.value.length || 0;
+});
+
